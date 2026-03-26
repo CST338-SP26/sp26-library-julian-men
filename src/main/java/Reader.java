af274af779639f53ed247fb853bb80397976ce97
Julian Mendoza <ianmenjones@gmail.com>
2026-03-26 13:15:13 -0700

    added : addBook, removeBook, hasBook, equals, hashCode, toString and placed in ReaderTest.java

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,3 +1,4 @@
+import Utilities.Code;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -71,5 +72,50 @@ public class Reader {
 
     public int getBookCount() {
         return books.size();
+    }
+
+    public Code addBook(Book book) {
+        if (hasBook(book)) {
+            return Code.BOOK_ALREADY_CHECKED_OUT_ERROR;
+        }
+        books.add(book);
+        return Code.SUCCESS;
+    }
+
+    public Code removeBook(Book book) {
+        if (!hasBook(book)) {
+            return Code.READER_DOESNT_HAVE_BOOK_ERROR;
+        }
+        if (books.remove(book)) {
+            return Code.SUCCESS;
+        }
+        return Code.READER_COULD_NOT_REMOVE_BOOK_ERROR;
+    }
+
+
+    public boolean hasBook(Book book) {
+        return books.contains(book);
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Reader reader = (Reader) o;
+        return cardNumber == reader.cardNumber &&
+                Objects.equals(name, reader.name) &&
+                Objects.equals(phone, reader.phone);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cardNumber, name, phone);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (#" + cardNumber + ") has checked out " + books;
     }
 }
