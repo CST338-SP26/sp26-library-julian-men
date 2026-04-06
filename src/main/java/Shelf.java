268c210a8afb9fcb251d527a5da481e075789b9d
Julian Mendoza <ianmenjones@gmail.com>
2026-04-06 13:46:17 -0700

    added getters, setters, equals, hashCode, toString

--- src/main/java/Shelf.java
+++ src/main/java/Shelf.java
@@ -39,4 +39,51 @@ public class Shelf {
         this.subject = subject;
         this.books = new HashMap<>();
     }
+
+
+    public int getShelfNumber() {
+        return shelfNumber;
+    }
+
+    public void setShelfNumber(int shelfNumber) {
+        this.shelfNumber = shelfNumber;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+
+    public HashMap<Book, Integer> getBooks() {
+        return books;
+    }
+
+    public void setBooks(HashMap<Book, Integer> books) {
+        this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Shelf shelf = (Shelf) o;
+        return shelfNumber == shelf.shelfNumber &&
+                Objects.equals(subject, shelf.subject);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(shelfNumber, subject);
+    }
+
+    @Override
+    public String toString() {
+        return shelfNumber + " : " + subject;
+    }
+
 }
